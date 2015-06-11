2b91cb2ca4dad2170d08ce8ed024c30f6d952676
Mia Garbagnati <f.g.garbagnati@gmail.com>
2015-06-10 22:18:45 -0700

    uses search controller from old unfinished project; functionality doesn't work

---
+++ js/controllers/search_controller.js
@@ -0,0 +1,23 @@
+NewMusic.App.SearchController = Ember.Controller.extend({
+  query: '',
+  echonest: new EchoNest('SNXAFW8HRV2RLZTNW'),
+  action: {
+    search: function() {
+      var artistName = this.get('query');
+      var searchArtist = this.getArtist(artistName);
+
+      console.log(serachArtist);
+    }
+  },
+
+  getArtist: function(artistName) {
+    return this.echonest.artist(artistName);
+  },
+
+  getSimilarArtists: function(artistName) {
+    var echonest = new EchoNest('SNXAFW8HRV2RLZTNW');
+    echonest.artist(artistName).similar(function(similarCollection) {
+      console.log(similarCollection.data.artists);
+    });
+  }
+});
