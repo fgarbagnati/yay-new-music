e2f2b2f3674fde6ef4015f08c56fc8e6e84f5836
Mia Garbagnati <f.g.garbagnati@gmail.com>
2015-06-10 22:36:09 -0700

    nothing's working, going to drink some wine and get back to this tomorrow

--- js/controllers/search_controller.js
+++ js/controllers/search_controller.js
@@ -4,20 +4,21 @@ NewMusic.App.SearchController = Ember.Controller.extend({
   action: {
     search: function() {
       var artistName = this.get('query');
-      var searchArtist = this.getArtist(artistName);
+      console.log(artistName)
+      // var searchArtist = this.getArtist(artistName);
 
-      console.log(serachArtist);
+      // console.log(serachArtist);
     }
   },
 
-  getArtist: function(artistName) {
-    return this.echonest.artist(artistName);
-  },
+  // getArtist: function(artistName) {
+  //   return this.echonest.artist(artistName);
+  // },
 
-  getSimilarArtists: function(artistName) {
-    var echonest = new EchoNest('SNXAFW8HRV2RLZTNW');
-    echonest.artist(artistName).similar(function(similarCollection) {
-      console.log(similarCollection.data.artists);
-    });
-  }
+  // getSimilarArtists: function(artistName) {
+  //   var echonest = new EchoNest('SNXAFW8HRV2RLZTNW');
+  //   echonest.artist(artistName).similar(function(similarCollection) {
+  //     console.log(similarCollection.data.artists);
+  //   });
+  // }
 });
